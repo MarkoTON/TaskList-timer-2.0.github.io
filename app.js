bfed0341cca497c9e4d2eff1cbd8c785f6f3c045
MarkoTON <MarkoTON@users.noreply.github.com>
2019-05-30 22:44:58 +0200

    Re-editing UI

--- app.js
+++ app.js
@@ -3,15 +3,13 @@ document.getElementById("taskBtn").addEventListener("click", getTask);
 // Creating Publick Array
 let allObjTask = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
 
-printUI();
-
 //// Get Value from HTML
 function getTask(){
   // Clear UL of all task before adding new one...
   document.getElementById("taskList").innerHTML = "";
   // Input value
   let getTask = document.getElementById("task").value;
-
+  
   if(getTask === null || getTask === "" || getTask === undefined) return false;
   // Timeduration for new task
   let timeDuration = document.getElementsByName("timeDuration");
@@ -29,9 +27,10 @@ function getTask(){
     task: getTask,
     time: getTimeStamp
   };
-
+  
   allObjTask.push(objTask);
-
+  
+  // Set LS
   localStorage.setItem('items', JSON.stringify(allObjTask));
   // Restarting Input field
   document.getElementById("task").value = "";
@@ -39,26 +38,22 @@ function getTask(){
   printUI();
 }
 
-// Seting Interval for checking timestamp
-setInterval(() => {
-  checkTime();
-}, 1000);
-
-function checkTime(){
-  for(let i = 0; i < allObjTask.length; i++){
-    // Converting timestamp
-    timeConverter(allObjTask[i].time);
-  }
-}
-
+printUI();
 // Printing UI from array
 function printUI(){
   let ul = document.getElementById("taskList");
-
+  
   allObjTask.forEach(element => {
     let li = document.createElement("li");
+    // let a = document.createElement("a");
+    // let delBtn = document.createElement("i");
     let text = document.createTextNode(element.task);
+    li.setAttribute("class","list-group-item");
+    // a.setAttribute("class", "delete-item d-inline");
+    // delBtn.setAttribute("class", "fas fa-times float-right mt-1");
+    // a.appendChild(delBtn);
     li.appendChild(text);
+    // li.appendChild(a);
     ul.appendChild(li);
   });
 }
@@ -69,7 +64,7 @@ function timeConverter(timeObj){
   let timestamp = timeObj;// timeObj is timestamp of current task
   // Converting time
   var a = new Date(timestamp);
-
+  
   var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
   var year = a.getFullYear();
   var month = months[a.getMonth()];
@@ -86,7 +81,7 @@ function timeConverter(timeObj){
   //////////////////////////////////////////////////////
   // Converting time
   var currentTime = new Date();
-
+  
   var currentMonths = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
   var currentYear = currentTime.getFullYear();
   var currentMonth = currentMonths[currentTime.getMonth()];
@@ -100,7 +95,7 @@ function timeConverter(timeObj){
   if( currentSec < 10 ) currentSec = "0" + currentSec;
   // Time format
   var currentTime = currentDate + ' ' + currentMonth + ' ' + currentYear + ' ' + currentHour + ':' + currentMin + ':' + currentSec;
-
+  
   // Calling function to remove task
   if(currentTime > time){
     removeTask(timestamp);
@@ -108,6 +103,7 @@ function timeConverter(timeObj){
 };
 
 //// Reoving Task from array and UI
+
 function removeTask(timestamp){
   // Restart UI
   document.getElementById("taskList").innerHTML = "";
@@ -126,6 +122,47 @@ function removeTask(timestamp){
   localStorage.setItem('items', JSON.stringify(allObjTask));
   // Print new UI
   printUI();
+}
+
+// var ulTaskList = document.querySelector("#taskList").addEventListener("click", removeTask);
+// // Remove Task
+// function removeTask(e) {
+//   // console.log(e);
+//   // if(e.parentElements === undefined) return;
+//   if(e.target.parentElement.classList.contains('delete-item')) {
+    
+//     if(confirm('Are You Sure?')) {
+//       e.target.parentElement.parentElement.remove();
+      
+//       // Remove from LS
+//       removeTaskFromLocalStorage(e.target.parentElement.parentElement);
+//     }
+//   }
+// }
+
+// function removeTaskFromLocalStorage(elementDel){
+//   // console.log(elementDel.textContent);
+//   allObjTask.forEach((element,index) => {
+//     // console.log(element.task)
+//     if(elementDel.textContent === element.task){
+//       allObjTask.splice(index, 1);
+//     }
+//   });
+  
+//   localStorage.setItem('items', JSON.stringify(allObjTask));
+//   checkTime();
+// }
+
+// Seting Interval for checking timestamp
+let timeInterval = setInterval(() => {
+  checkTime();
+}, 1000);
+
+function checkTime(){
+  for(let i = 0; i < allObjTask.length; i++){
+    // Converting timestamp
+    timeConverter(allObjTask[i].time);
+  }
 }
 
 // Clear all task from Array and UI
